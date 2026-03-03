fa51678831505228e7374e053fcb8de379b71394
Jafeth Raul <jafethraul102@gmail.com>
2026-03-02 19:37:27 -0600

    menu agregado

--- Buscaminas/main.cpp
+++ Buscaminas/main.cpp
@@ -14,20 +14,89 @@ struct Usuario {
     int puntuacion;
 };
 
-int opcion;
+int opcion, opcion2, opcion3;
 
 int main()
 {
+    do {
         cout << "--BUSCAMINAS--" << endl;
         cout << "1)login" << endl;
         cout << "2)registrar" << endl;
         cout << "3)salir." << endl;
+        cin >> opcion;
 
+        switch(opcion)
+        {
+        case 1:
+        case 2:
 
+            do {
+                cout << "--BUSCAMINAS--" << endl;
+                cout << "1)PARTIDAS." << endl;
+                cout << "2)Rankin." << endl;
+                cout << "3)salir." << endl;
+                cin >> opcion2;
 
+                switch(opcion2)
+                {
+                case 1:
+                    do {
+                        cout << "1) Partida niveles secuenciales" << endl;
+                        cout << "2) partida Facil" << endl;
+                        cout << "3) Partida Normal." << endl;
+                        cout << "4) Partida Dificil" << endl;
+                        cin >> opcion3;
+
+                        switch(opcion3)
+                        {
+                        case 1:
+                            cout << "Elegiste niveles secuenciales\n";
+                            break;
+                        case 2:
+                            cout << "Elegiste partida facil\n";
+                            break;
+                        case 3:
+                            cout << "Elegiste partida normal\n";
+                            break;
+                        case 4:
+                            cout << "Elegiste partida dificil\n";
+                            break;
+                        case 5:
+                            break;
+                        default:
+                            cout << "Opcion invalida\n";
+                        }
+
+                    } while (opcion3 != 5);
+                    break;
+
+                case 2:
+                    cout << "Mostrando ranking...\n";
+                    break;
+
+                case 3:
+                    break;
+
+                default:
+                    cout << "Opcion invalida\n";
+                }
+
+            } while (opcion2 != 3);
+            break;
+
+        case 3:
+            cout << "Saliendo del sistema...\n";
+            break;
+
+        default:
+            cout << "Opcion invalida\n";
+        }
+
+    } while (opcion != 3);
 
     return 0;
 }
+
 /*
     //MENU1
     cout << "--BUSCAMINAS--" << endl;
